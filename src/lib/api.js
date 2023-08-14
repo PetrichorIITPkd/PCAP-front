d3e2027443673bf50662d7445e64c99610a6af03
enchanteddev <ex@example.com>
2023-08-14 23:16:53 +0530

    apichanged

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://127.0.0.1:8000/api"
+export const API_URL = "https://pcap-back-production.up.railway.app/api"
 export const API = {
     login: "/login/",
     register: "/register/",

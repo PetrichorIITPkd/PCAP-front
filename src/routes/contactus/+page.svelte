3f29a15d0170570b6e6b83f9f536412046024ded
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2023-08-22 23:47:57 +0530

    qikfix

--- src/routes/contactus/+page.svelte
+++ src/routes/contactus/+page.svelte
@@ -1,12 +1,12 @@
 <h1>Get In Touch!</h1>
-<h2>Media and Publicity Heads</h2>
+<h2>Learn More about the CA Programme</h2>
 <p>Everything to do with the CA program, its details, rewards, and much more.</p>
 <p><strong>Ashwin R. Nair</strong>: +91 91881 42004</p>
 <p><strong>Nikhil S Thomas</strong>: +91 85901 18650</p>
-<h2>Web Development Heads</h2>
+<!-- <h2>Web Development Heads</h2>
 <p>Everything to do with the website, technical queries, and other stuff.</p>
 <p><strong>Kaushik Rawat</strong>: +91 72087 98365</p>
-<p><strong>Kshitij Ghodake</strong>: +91 72989 91001</p>
+<p><strong>Kshitij Ghodake</strong>: +91 72989 91001</p> -->
 <style>
     h1{
         margin-bottom: 0;

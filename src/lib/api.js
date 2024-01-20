5e273db0e960eb8640ac30496a96c3cea63c4af4
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2024-01-21 03:33:35 +0530

    api name changes

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,8 +1,8 @@
-export const API_URL = "https://pcap-back-production.up.railway.app/api"
-// export const API_URL = "http://127.0.0.1:8000/api"
+export const API_URL = "https://pcap-back-production.up.railway.app/"
+// export const API_URL = "http://127.0.0.1:8000/"
 export const API = {
-    login: "/login/",
-    register: "/register/",
-    user_data: "/user/",
-    admin: "/admin/"
+    login: "new/login/",
+    register: "new/register/",
+    user_data: "api/user/",
+    admin: "api/admin/"
 }
